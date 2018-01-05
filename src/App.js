d41d6de3f23a68dc8bc89e128e03561f65a8c86b
Karl Lääts <karl.laats@transferwise.com>
2018-01-05 10:16:29 +0200

    Improve navbar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div>
+				<div className="App">
 					<NavBar />
 					<Switch>
 						<Route exact path="/" component={Home} />
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import './NavBar.css';
+import logo from '../../logo.svg';
+
+const options = [
+	{
+		label: 'Test',
+		icon: null,
+		to: '/test',
+	},
+	{
+		label: 'Random',
+		icon: null,
+		to: '/asd',
+	},
+];
 
 class NavBar extends Component {
 	render() {
-		return <div className="NavBar">NavBar</div>;
+		return (
+			<div className="NavBar">
+				<Link to="/">
+					<div className="NavBar-logo">
+						<img src={logo} alt="React logo" />
+					</div>
+				</Link>
+				{options.map(this.renderOption)}
+			</div>
+		);
 	}
+
+	renderOption = (option, index) => {
+		return (
+			<Route
+				key={index}
+				path={option.to}
+				children={({ match }) => (
+					<Link to={option.to}>
+						<div className={'NavBar-option ' + (match ? 'active' : '')}>{option.label}</div>
+					</Link>
+				)}
+			/>
+		);
+	};
 }
 
 export default NavBar;
